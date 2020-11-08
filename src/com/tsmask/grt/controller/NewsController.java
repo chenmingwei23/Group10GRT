ad64295d7ca9742365ba9f7780e3f349543c80dc
Xiang Weng <wxwhyain@gmail.com>
2020-11-08 23:13:02 +0800

    Missing one annotation

--- src/com/tsmask/grt/controller/NewsController.java
+++ src/com/tsmask/grt/controller/NewsController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller
 public class NewsController {
 	private static final Logger logger = LoggerFactory.getLogger(NewsController.class);
 	@RequestMapping(value = "/News", method = RequestMethod.GET)
